2b4ce948bb73ccf10c10f697b944b4872062c342
Pusparum Murih <murih.pusparum@vito.be>
2021-11-22 18:32:41 +0100

    remove unnecessary packages

--- codes/install.package.R
+++ codes/install.package.R
@@ -1,2 +1,1 @@
-install.packages(c("quantreg", "invgamma", "lqmm", "readr", "ggplot2", "tidyverse", "reshape2", "lsa",
-                   "devtools", "circlize", "tidyr", "caret", "gridBase"))
+install.packages(c("quantreg", "invgamma", "lqmm", "readr", "ggplot2", "tidyverse", "reshape2", "tidyr"))
